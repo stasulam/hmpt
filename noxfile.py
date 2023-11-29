fc361b398f5c04c0addb46d5e392e85a3b0a9c5a
lukasz <lukaszambroziak@MacBook-Air-Lukasz.local>
2023-11-29 22:49:22 +0100

    add: nox.

---
+++ noxfile.py
@@ -0,0 +1,8 @@
+import nox
+
+
+@nox.session(python=["3.12"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *args)
