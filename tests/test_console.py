3756c97bf74aa6127f73162e071b951c542ad5d2
lukasz <lukaszambroziak@MacBook-Air-Lukasz.local>
2023-12-01 20:23:57 +0100

    add: tests.

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,6 @@
 import click.testing
 import pytest
+import requests
 
 from hmpt import console
 
@@ -8,7 +9,32 @@ from hmpt import console
 def runner():
     return click.testing.CliRunner()
 
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("hmpt.wikipedia.random_page")
 
-def test_main_succeeds(runner):
+
+def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+
+
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert "Error" in result.output
+
+
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
